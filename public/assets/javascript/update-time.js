534cab74c89589f8edda3b733aa5e0572c4b7555
iiTONELOC <anthonytropeano@me.com>
2021-05-09 22:36:57 -0400

    serve time was off, corrected issue

--- public/assets/javascript/update-time.js
+++ public/assets/javascript/update-time.js
@@ -1,17 +1,19 @@
-// TIME CONSTANTS
-
+// DATE&TIME CONSTANTS
+const dateEl = document.getElementById('date');
 const timeEl = document.getElementById('time');
 
-// SET TIME
+// SET DATE&TIME
 
+const date = moment(new Date()).format('dddd [the] Do [of] MMMM');
 const time = moment(new Date()).format('LTS');
 
-
+dateEl.textContent = date;
+timeEl.textContent = time;
 
 function updateTime() {
     setInterval(function () {
-        const updatedTime = moment(new Date()).format('LTS')
-        timeEl.textContent = updatedTime
+        const updatedTime = moment(new Date()).format('LTS');
+        timeEl.textContent = updatedTime;
     }, 500)
 }
 updateTime();

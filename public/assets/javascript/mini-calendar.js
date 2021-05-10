d46c68d5b22c3314c13d95d924d5a2c3a0165ace
iiTONELOC <anthonytropeano@me.com>
2021-05-10 03:51:22 -0400

    fixed issue with nonevents displaying

--- public/assets/javascript/mini-calendar.js
+++ public/assets/javascript/mini-calendar.js
@@ -8,12 +8,14 @@ currentDayEl.setAttribute('class', 'mini-current-date')
 // get list of days with events
 const eventData = document.querySelector(".mini-month").getAttribute('data-events');
 let eventArray = eventData.split(" ")
-if(eventArray.length>0){
+console.log(eventArray)
+if(eventArray.length>=1){
     for (let i = 0; i < eventArray.length; i++) {
         const element = eventArray[i];
-        if(element === 0){
-            element = ''
+        if(element === 0 || element === ' '|| element === null || element === undefined|| element === ''){
+            break
         }
+        console.log(element)
         document.getElementById(`mini-date-${element}`).setAttribute('class', 'current-event')
     }
 }

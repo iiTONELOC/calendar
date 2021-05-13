a38d8d4e3192523ccb672382ac08ccf77ac94856
iiTONELOC <anthonytropeano@me.com>
2021-05-13 14:49:37 -0400

    force false

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(routes);
 
 
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on ${PORT}`))
 })
     .then(async () => {
